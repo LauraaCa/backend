fdd8cbbc7feb5b254a04ed1515316909de530688
LauraaCa <lauracastanedar962@gmail.com>
2025-03-28 15:10:46 -0500

    feature(bkend) : agregar DTO y mejora bd

---
+++ src/main/java/dto/EquipoDto.java
@@ -0,0 +1,10 @@
+package dto;
+
+import org.apache.logging.log4j.core.config.plugins.validation.constraints.NotBlank;
+
+import java.time.LocalDateTime;
+
+public record EquipoDto (@NotBlank String nombre,
+                         String nombreCorto,
+                         LocalDateTime fechaCreacion){
+}
